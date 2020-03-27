67709836392887de917dc7000d7b58755f926a40
Jiaao Yu <yu.jiaa@husky.neu.edu>
2020-03-27 18:14:59 -0400

    test for 15 total war lines
    ttw foreach println

--- SteamRating/src/main/scala/SteamDataCleansing.scala
+++ SteamRating/src/main/scala/SteamDataCleansing.scala
@@ -4,6 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SteamDataCleansing {
   def main(args: Array[String]) {
     //linux files or sth like HDFS
+    //edit configurations => program arguments: ./Steam.csv
         if (args.length == 0) {
           System.err.println("Usage: SparkWordCount <inputfile>")
           System.exit(1)
@@ -11,15 +12,15 @@ object SteamDataCleansing {
 
     //run locally, and set up number of threads
     //e.g. setMaster("local[2]")
-    val conf = new SparkConf().setAppName("SteamDataCleansing").setMaster("local[" + Runtime.getRuntime.availableProcessors() + "]")
+    val conf = new SparkConf().setAppName("SteamDataCleansing").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
     //wordcount: counting number of lines containing "spark"
     //    val count = sc.textFile(args(0)).filter(line => line.contains("Spark")).count()
     //    println("count=" + count)
 
-    val ttw = sc.textFile(args(0)).filter(_.contains("Total War")).count()
-    println(ttw)
+    val ttw = sc.textFile(args(0)).filter(_.contains("Total War")).collect()
+    ttw foreach println
     sc.stop()
   }
 }
