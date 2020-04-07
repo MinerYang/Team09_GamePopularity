a42aba44c83378853449dc46c5042eabd811fc0e
Jiaao Yu <yu.jiaa@husky.neu.edu>
2020-04-07 18:12:20 -0400

    basic multi-classification ml frame added

---
+++ SteamRating/src/main/scala/ml/NaiveBayesML.scala
@@ -0,0 +1,40 @@
+package ml
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+case object NaiveBayesML {
+  lazy val appName = "SteamDataCleansing"
+  lazy val master = "local[*]"
+
+  def main(args: Array[String]) {
+    val ss = SparkSession.builder.master(master).appName(appName).getOrCreate()
+    ss.sparkContext.setLogLevel("WARN")
+
+    // Load data
+    val data = ss.read.format("libsvm")
+      .load("hdfs://localhost:9000/CSYE7200/steam-data-for-ml")
+
+    // Split the data into training and test sets (30% held out for testing)
+    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3), seed = 1234L)
+
+    // Train a NaiveBayes model.
+    val model = new NaiveBayes()
+      .fit(trainingData)
+
+    // Select example rows to display.
+    val predictions = model.transform(testData)
+    predictions.show()
+
+    // Select (prediction, true label) and compute test error
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println(s"Test set accuracy = $accuracy")
+  }
+}
+
+
