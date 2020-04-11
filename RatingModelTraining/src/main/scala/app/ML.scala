edd6240a0b4c39b80d879cd2f6465fd841530a42
Jiaao Yu <yu.jiaa@husky.neu.edu>
2020-04-11 13:22:17 -0400

    ml object added

---
+++ RatingModelTraining/src/main/scala/app/ML.scala
@@ -0,0 +1,19 @@
+package app
+import org.apache.spark.ml.classification.{_}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+
+object ML {
+  def lr
+
+  def ovr
+
+  def rf
+
+  def mlp
+
+  def nb
+
+
+}
